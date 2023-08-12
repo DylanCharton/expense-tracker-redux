49cdbd0e0055f5e5a111278acfbde5f794eb540a
DylanCharton <d.charton@codeur.online>
2023-08-12 22:50:09 +0200

    imported redux and first slice

---
+++ src/store/expense/expense-slice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const expenseSlice = createSlice({
+  name: "expenseSlice",
+  initialState: {
+    expenseList: [],
+  },
+  reducers: {
+    addExpense: (currentSlice, action) => {
+      currentSlice.expenseList.push(action.payload);
+      console.log("addExpense", action);
+    },
+  },
+});
+
+// destructuration
+const { addExpense } = expenseSlice.actions;
+
+export { addExpense };
